9d6b93d4ece982fdecc5f072eb7db8460e91921b
Suthahar <jssuthahar@gmail.com>
2025-06-12 21:51:13 +0800

    Amazon Project

---
+++ Amazon/MiniSuperMarket/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniSuperMarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
---
+++ Amazon/MiniSuperMarket/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniSuperMarket.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
