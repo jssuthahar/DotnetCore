366b4f726d902022c81ba8ae4c7c97c4280ff542
Suthahar <jssuthahar@gmail.com>
2025-06-13 21:45:29 +0800

    ViewBag, ViewData

--- Amazon/MiniSuperMarket/Controllers/HomeController.cs
+++ Amazon/MiniSuperMarket/Controllers/HomeController.cs
@@ -6,6 +6,15 @@ namespace MiniSuperMarket.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Location"] = "Bangalore";
+            ViewData["Product"]= new List<string> { "Laptop",
+                "Mobile",
+                "Computer",
+                "Camera"
+            };
+            ViewBag.FilterOptions = new List<string> { "All", "All Categories", "Amazon Devices", "Next Day Delivery","Same Day Delivery" };
+
+
             return View();
         }
         public IActionResult Product()
