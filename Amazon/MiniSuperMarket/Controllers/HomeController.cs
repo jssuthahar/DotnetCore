9d49ff1cd607cdbcb59d8fdda6a41a9bda98912d
Suthahar <jssuthahar@gmail.com>
2025-06-16 21:55:43 +0800

    updated

--- Amazon/MiniSuperMarket/Controllers/HomeController.cs
+++ Amazon/MiniSuperMarket/Controllers/HomeController.cs
@@ -13,12 +13,20 @@ namespace MiniSuperMarket.Controllers
                 "Camera"
             };
             ViewBag.FilterOptions = new List<string> { "All", "All Categories", "Amazon Devices", "Next Day Delivery","Same Day Delivery" };
-
+            TempData["Message"] = "Welcome to Mini Super Market!";
+            TempData["ProductCount"] = 5;
 
             return View();
         }
         public IActionResult Product()
         {
+            ViewData["Location"] = "Bangalore";
+            ViewData["Product"] = new List<string> { "Laptop",
+                "Mobile",
+                "Computer",
+                "Camera"
+            };
+            ViewBag.FilterOptions = new List<string> { "All", "All Categories", "Amazon Devices", "Next Day Delivery", "Same Day Delivery" };
             return View();
         }
     }
---
+++ Amazon/MiniSuperMarket/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniSuperMarket.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
